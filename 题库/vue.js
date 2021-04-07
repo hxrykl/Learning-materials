779b15f157387b1a58070e31ca2893148d37136f
= <1574551580@qq.com>
2021-04-08 00:59:47 +0800

    浏览器缓存

--- 题库/vue.js
+++ 题库/vue.js
@@ -18,3 +18,18 @@ vue项目的优化
 vue生命周期
 
 说一下父组件和子组件之间的通信
+	prop、$emit、
+
+	$attrs、$listeners：
+		子组件定义v-bind="$attrs"获取非prop的父组件在子组件设置的属性
+		inheritAttrs
+
+	provide、inject、，父组件通过provide提供变量，子孙组件通过inject注入使用变量，轻松实现跨级访问祖先组件的数据
+
+	EventBus.$on()监听 EventBus.$emit()分发 EventBus.$off()取消监听
+
+	Vuex:state、getter、mutation、action
+
+	this.$parent访问该组件的父组件、this.$children访问该组件的子组件
+
+	$root设置根
