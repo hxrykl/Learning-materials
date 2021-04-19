b22c8526679b5ea3d4840dc5c19b9cf477741fa0
= <1574551580@qq.com>
2021-04-20 01:02:38 +0800

    手撕instanceof

--- 题库/backOriginCode.js
+++ 题库/backOriginCode.js
@@ -5,6 +5,14 @@
 实现一个bind
 
 实现一个instanceof
-	
+	function newInstanceof(left, right) {
+		let proto = left.__proto__
+		let prototype = right.prototype
+		while(true){
+			if(proto === null) return false
+			if(proto === prototype) return true
+			proto = proto.__proto__//往上找
+		}
+	}
 
 实现深浅拷贝
